191252283bd3600cf79776467eb04c412c9ccf33
Mariam-2000 <62655495+Mariam-2000@users.noreply.github.com>
2020-03-31 18:42:42 +0300

    Removed scanner from the code

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -1,9 +1,6 @@
-import java.util.Scanner;
 public class EvenOdd {
     public static void main(String[] args) {
-        Scanner input = new Scanner (System.in);
-        System.out.println("Please enter a number");
-        int num= input.nextInt();
+        int num= 6;
         if ( num % 2 == 0 )
         System.out.println("Entered number is even");
         else
